af3ac90c0f871df1dc759f91d9f9dbb925a6c8b6
René Widera <r.widera@hzdr.de>
2025-06-12 16:38:19 +0200

    07 with cublas

--- example/sgemm/src/05_smem_thread_oversubscription_nonQuadratic.cpp
+++ example/sgemm/src/05_smem_thread_oversubscription_nonQuadratic.cpp
@@ -75,12 +75,6 @@ struct SMemThreadOversubscriptionNonQuadraticKernel
                     sharedATile,
                     A);
 
-                for(auto tileElemIndexMD : onAcc::makeIdxMap(acc, onAcc::worker::threadsInBlock, IdxRange{sBExtent}))
-                {
-                    sharedBTile[tileElemIndexMD]
-                        = B[Vec2D{tileElemIndexMD.y() + chunkOffset, tileOffsetMD.x() + tileElemIndexMD.x()}];
-                }
-
                 simdGrid.template concurrent<16u, Alignment<16>>(
                     acc,
                     sBExtent,
