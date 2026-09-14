58a0dcf0104a934db0982285ec02437ab8727348
René Widera <r.widera@hzdr.de>
2026-09-14 18:10:31 +0200

    apply review comments

--- script/job_generator/src/alpaka_bashi/ci_yaml/images.py
+++ script/job_generator/src/alpaka_bashi/ci_yaml/images.py
@@ -17,6 +17,8 @@ from bashi.globals import (
     OFF_VER,
     UBUNTU,
 )
+
+from packaging.version import Version
 from typeguard import typechecked
 
 from alpaka_bashi.utils import print_warn
@@ -26,23 +28,13 @@ GITLAB_IMAGE_CACHE: list[str] | None = None
 # is used to display missing image warning one time
 image_warning_cache: list[str] = []
 
-CUSTOM_ROCM_IMAGES: dict[str, str] = {
-    # The alpaka CI container registry does not provide a prebuilt ROCm 10.0 image yet.
-    # Creating the environment on a base alpaka ubuntu container is also not possible due to
-    # missing apt packages.
-    # Use AMD's official ROCm development image directly from Docker Hub.
-    "10.0": "rocm/dev-ubuntu-24.04:10.0.0-full",
-}
-
 
 @typechecked
 def get_custom_image_name(combination: bashi.Combination) -> str:
     """Return a custom CI image for combinations not covered by the alpaka CI registry."""
-    if combination[ALPAKA_ACC_GPU_HIP_ENABLE].version != OFF_VER:
-        return CUSTOM_ROCM_IMAGES.get(
-            str(combination[DEVICE_COMPILER].version),
-            "",
-        )
+
+    if combination[ALPAKA_ACC_GPU_HIP_ENABLE].version == Version("10.0"):
+        return "rocm/dev-ubuntu-24.04:10.0.0-full"
 
     return ""
 
@@ -104,9 +96,6 @@ def get_image_name(combination: bashi.Combination, container_version: str) -> st
     Returns:
         str: image name
     """
-    if custom_image_name := get_custom_image_name(combination):
-        return custom_image_name
-
     image_name = get_base_image(combination[UBUNTU].version)
 
     cuda_ver = combination[ALPAKA_ACC_GPU_CUDA_ENABLE].version
@@ -136,9 +125,6 @@ def get_existing_image(image_name: str, combination: bashi.Combination, containe
     Returns:
         str: If image exist, return `image_name`. Otherwise return base image name.
     """
-    if custom_image_name := get_custom_image_name(combination):
-        return custom_image_name
-
     gitlab_images = get_images_from_registry(container_version)
     if image_name in gitlab_images:
         return image_name
@@ -163,6 +149,10 @@ def set_image(
         job_body (Dict[str, Any]): GitLab CI test job body yaml
         combination (bashi.Combination): combination
     """
+    if custom_image_name := get_custom_image_name(combination):
+        job_body["image"] = custom_image_name
+        return
+
     image_name = get_image_name(combination, container_version)
 
     if image_check:
